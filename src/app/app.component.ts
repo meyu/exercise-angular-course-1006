72d9e3d99e969e2d03db8ed9a5f387883616ad8e
meyu <yrrvknone@gmail.com>
2017-10-06 17:22:22 +0800

    實作全選功能
    * isDone 改用 foreach 方法來完成
    * 解除 footer 的隱藏，使 clear 功能在全選後仍能發揮作用

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,34 +9,45 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   todos: ItemTodo[] = [];
+  catAll: boolean = true;
+
 
   // TODO: 不明所以，清單在套用 descTodos Pipe 後，沒有這個方法，則清單不會更新顯示；footer 的待辦統計亦同。
   triggerViewBind() {
     this.todos = this.todos.slice();
   }
   genUniqID(): string {
-    return Math.random().toString(36).substr(2,9);
+    return Math.random().toString(36).substr(2, 9);
   }
 
 
-  
+
 
   addTodo(newTodo: string) {
     if (newTodo !== '') {
-        this.todos.push({idx: this.genUniqID(), todo: newTodo,isDone: false});
-        this.triggerViewBind();
+      this.todos.push({ idx: this.genUniqID(), todo: newTodo, isDone: false });
+      this.triggerViewBind();
     }
   }
 
-  notTodo(item: ItemTodo) {
-    this.todos = this.todos.map(function(i){
+
+  notTodo(item?: ItemTodo) {
+    this.todos.forEach(i => {
       if (i.idx == item.idx) {
         i.isDone = item.isDone;
       }
-      return i;
     })
     this.triggerViewBind();
   }
+
+  notTodoAll() {
+    this.todos.forEach(item => {
+      item.isDone = this.catAll;
+    })
+    this.triggerViewBind();
+    this.catAll = !this.catAll;
+  }
+
 
   rmvTodo(item: ItemTodo) {
     this.todos = this.todos.filter(it => it !== item);
